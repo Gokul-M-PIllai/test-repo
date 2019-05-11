683751953014c1099c07be0d91589bf50f9065f4
Gokul-M-PIllai <50300946+Gokul-M-PIllai@users.noreply.github.com>
2019-05-11 23:52:50 +0530

    table inserts

--- test2.sql
+++ test2.sql
@@ -6,4 +6,8 @@ set echo on
 
 create table test(no number,text varchar2(100));
 
+insert into test(1,'Line 1');
+insert into test(2,'Line 2');
+commit;
+                                          
 exit;
